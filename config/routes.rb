b53f9ffb9aca656842ae4167600fb041e70cb53d
Ryan <ryanjdayrit@intelimina.com>
2017-04-27 18:51:05 +0800

    Finish sign-up page

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
   root 'static_pages#home'
   get  '/signup',  to: 'users#new'
   post '/signup',  to: 'users#create'
   resources :users
-=======
-  root 'application#login'
->>>>>>> sign-up
 end
